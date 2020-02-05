99a42500a345ac46c5be375c2f8a0f3ad16b926a
Václav Kasal <vasekkasal@gmail.com>
2020-02-06 00:00:00 +0100

    sql initilization added

---
+++ roles/postgresql/files/initialize.sql
@@ -0,0 +1,6 @@
+CREATE TABLE tablefullofid (id int);
+INSERT INTO tablefullofid VALUES(1);
+INSERT INTO tablefullofid VALUES(2);
+INSERT INTO tablefullofid VALUES(3);
+INSERT INTO tablefullofid VALUES(4);
+INSERT INTO tablefullofid VALUES(5);
